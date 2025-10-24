f12b366f24d5f8e813a5bdaa70c6459053ae1896
Daksh <dakshablram@gmail.com>
2025-10-24 17:33:28 -0400

    Question 32

--- Automaton.java
+++ Automaton.java
@@ -49,14 +49,16 @@ public class Automaton
         // Build the new state in a separate array.
         // Naively update the state of each cell
         // based on the state of its two neighbors.
+        int previousLeft = 0;
         for(int i = 0; i < state.length; i++) {
             int left, center, right;
             left = (i == 0) ? 0 : state[i-1];
             center = state[i];
             right = (i + 1 < state.length) ? state [i + 1] : 0;
-            state[i] = (left + center + right) % 2;
+            state[i] = (previousLeft + center + right) % 2;
+            previousLeft = center;
         }
-        state = state;
+        
     }
     
     /**
